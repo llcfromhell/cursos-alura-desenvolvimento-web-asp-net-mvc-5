66c4ceb9021ead1f264c9dbabce95a2d0ff99195
llcfromhell <llc.from.hell@gmail.com>
2016-10-23 23:35:12 -0200

    aulas 02 03 e 04

---
+++ CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            ViewBag.categorias = new CategoriasDAO().Lista();
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {
+            new CategoriasDAO().Adiciona(categoria);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,51 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+
+            return View(produtos);
+        }
+
+        public ActionResult Form()
+        {
+
+            ViewBag.categorias = new CategoriasDAO().Lista();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            new ProdutosDAO().Adiciona(produto);
+
+            return RedirectToAction("IndexViewBag");
+        }
+
+        public ActionResult IndexViewBag()
+        {
+
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+
+            ViewBag.produtos = produtos;
+
+            return View();
+        }
+    }
+}
