6f7664e390462e5ce6006bf95fc320097cbc8ee4
llcfromhell <llc.from.hell@gmail.com>
2016-11-02 18:59:28 -0200

    aula 09

--- CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -78,7 +78,7 @@ namespace CaelumEstoque.Controllers
             Produto produto = dao.BuscaPorId(id);
             produto.Quantidade--;
             dao.Atualiza(produto);
-            return RedirectToAction("Index");   
+            return Json(produto);   
         }
     }
 }
