4accfef2ca9f8e4d617865d6f1edb3906478ca76
llcfromhell <llc.from.hell@gmail.com>
2016-11-02 21:30:37 -0200

    aula 10

---
+++ CaelumEstoque/CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            object usuario = Session["usuarioLogado"];
+
+            if (usuario == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToRoute("ListaProdutos");
+            }
+        }
+
+        public ActionResult Autenticar(string login, string senha)
+        {
+            UsuariosDAO dao = new UsuariosDAO();
+
+            Usuario usuario = dao.Busca(login, senha);
+
+            if (usuario == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToRoute("ListaProdutos");
+            }
+
+            return View();
+        }
+    }
+}
